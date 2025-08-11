1cdc651f894c87929d0aab3cc7656e57c459ecd5
Pragyesh Kumar Seth <spragyesh86@gmail.com>
2025-08-11 11:33:47 +0530

    Add friend request functionality with validation and model setup

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import FriendRequest from "../models/FriendRequest.js";
 import User from "../models/User.js";
 
 export async function getRecommendedUsers(req, res) {
@@ -28,6 +29,55 @@ export async function getMyFriends(req, res) {
         res.status(200).json(user.friends);
     } catch (error) {
         console.error("Error in getMyFriends controller:", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export async function sendFriendRequest(req, res) {
+    try {
+        const myId = req.user.id;
+        const { id: recipientId } = req.params;
+
+        // Prevent sending request to yourself
+        if (myId === recipientId) {
+            return res.status(400).json({ message: "You can't send a friend request to yourself." });
+        }
+
+        // Check if the recipient exists
+        const recipient = await User.findById(recipientId);
+        if (!recipient) {
+            return res.status(404).json({ message: "Recipient not found." });
+        }
+
+        // Check if the recipient is already a friend
+        if (recipient.friends.includes(myId)) {
+            return res.status(400).json({ message: "You are already friends with this user." });
+        }
+
+        // Check if a request already exists
+        const existingRequest = await FriendRequest.findOne({
+            $or: [
+                { sender: myId, recipient: recipientId },
+                { sender: recipientId, recipient: myId }
+            ]
+        });
+
+        if (existingRequest) {
+            return res.status(400).json({ message: "A friend request already exists between you and this user." });
+        }
+
+        // If all checks are done, Create a new friend request
+        const friendRequest = await FriendRequest.create({
+            sender: myId,
+            recipient: recipientId
+        })
+
+        res.status(201).json({
+            message: "Friend request sent successfully.",
+            friendRequest
+        });
+    } catch (error) {
+        console.error("Error in sendFriendRequest controller:", error.message);
         res.status(500).json({ message: "Internal server error" });
     }
 }
