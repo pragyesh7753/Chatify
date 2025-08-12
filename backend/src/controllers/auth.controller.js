0c425df9d10307d4636ee678082e12ea0dca3964
Pragyesh Kumar Seth <spragyesh86@gmail.com>
2025-08-12 17:41:12 +0530

    fix: update cookie settings to allow cross-site cookies for authentication

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -53,7 +53,7 @@ export async function signup(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
+      sameSite: "none", // Allow cross-site cookies for different domains
       secure: process.env.NODE_ENV === "production",
     });
 
@@ -85,7 +85,7 @@ export async function login(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
+      sameSite: "none", // Allow cross-site cookies for different domains
       secure: process.env.NODE_ENV === "production",
     });
 
@@ -97,7 +97,10 @@ export async function login(req, res) {
 }
 
 export function logout(req, res) {
-  res.clearCookie("jwt");
+  res.clearCookie("jwt", {
+    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
+  });
   res.status(200).json({ success: true, message: "Logout successful" });
 }
 
